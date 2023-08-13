431ee59c8b2854558af0161d339e01ba78eb01a4
BingXuanOwO <hxc35459@outlook.com>
2023-08-14 01:32:54 +0800

    feat(test): add id3v2.3 testing files

--- test/audio_metadata_parser_test.dart
+++ test/audio_metadata_parser_test.dart
@@ -1,12 +1,19 @@
+import 'dart:io';
+
 import 'package:flutter_test/flutter_test.dart';
 
 import 'package:audio_metadata_parser/audio_metadata_parser.dart';
 
 void main() {
-  test('adds one to input values', () {
-    final calculator = Calculator();
-    expect(calculator.addOne(2), 3);
-    expect(calculator.addOne(-7), -6);
-    expect(calculator.addOne(0), 1);
+  test("test id3v2.3", () {
+  final parser = AudioMetadataParser(File("test/id3_23.bin"));
+    final metadata = parser.parse();
+    expect(metadata.title, "TrackName");
+    expect(metadata.artist, "SongArtist");
+    expect(metadata.album, "AlbumName");
+    expect(metadata.albumArtist, "AlbumArtist");
+    expect(metadata.year, 2000);
+    expect(metadata.trackNumber, 5);
+    expect(metadata.albumTrackCount, 6);
   });
 }
