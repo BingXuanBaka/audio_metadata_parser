d310e55ac02e8d37640611c96fd5d87ffd86abc3
BingXuanOwO <hxc35459@outlook.com>
2023-08-20 03:00:39 +0800

    chore: edit test file

--- test/audio_metadata_parser_test.dart
+++ test/audio_metadata_parser_test.dart
@@ -6,10 +6,10 @@ import 'package:audio_metadata_parser/audio_metadata_parser.dart';
 
 void main() {
   test("test id3v2.3", () {
-  final parser = AudioMetadataParser(File("test/id3_23.bin"));
+    final parser = AudioMetadataParser(File("test/id3_23.bin"));
     final metadata = parser.parse();
     expect(metadata.title, "TrackName");
-    expect(metadata.artist, "SongArtist");
+    expect(metadata.artist, ["SongArtist"]);
     expect(metadata.album, "AlbumName");
     expect(metadata.albumArtist, "AlbumArtist");
     expect(metadata.year, 2000);
